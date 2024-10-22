69b99d78ea6936966a07b6af7053ee10d90269ba
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-10-22 22:45:10 +0200

    codigo testeado con pruebas unitarias

--- src/main/java/org/factoriaf5/App.java
+++ src/main/java/org/factoriaf5/App.java
@@ -1,9 +1,53 @@
 package org.factoriaf5;
 
-public class App 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App
 {
+    static String input;
+    static Double peso;
+    static Double altura;
     public static void main( String[] args )
     {
-        
+        try(Scanner sc = new Scanner(System.in).useLocale(Locale.US)){
+            System.out.println("Introducir peso en kg");
+            input = sc.next();
+            while(!esDoublePositivo(input)){
+                System.out.println("Input no valido. Introduzca el peso de nuevo");
+                input = sc.next();
+            }
+            peso = Double.valueOf(input);
+            System.out.println("Introducir altura en metros");
+            input = sc.next();
+            while(!esDoublePositivo(input)){
+                System.out.println("Input no valido. Introduzca la altura de nuevo");
+                input = sc.next();
+            }
+            altura = Double.valueOf(input);
+            System.out.println("Tu indice de IMC es "+calcularIMC(peso,altura));
+            System.out.println("Evaluacion del resultado: "+evaluarResultadoIMC(calcularIMC(peso,altura)));
+        }
+    }
+    public static Double calcularIMC(Double peso,Double altura){
+        return Math.floor((peso/Math.pow(altura, 2))*100)/100;
+    }
+    public static String evaluarResultadoIMC(Double imc){
+        return imc<16?"delgadez severa"
+        :imc<17?"delgadez moderada"
+        :imc<18.5?"delgadez leve"
+        :imc<25?"peso normal"
+        :imc<30?"sobrepeso"
+        :imc<35?"obesidad leve"
+        :imc<40?"obesidad moderada"
+        :"obesidad morbida";
+    }
+
+    public static boolean esDoublePositivo(String input){
+        try{
+            return Double.parseDouble(input)>0;
+        }catch(NumberFormatException e){
+            return false;
+        }
     }
 }
--- src/test/java/org/factoriaf5/AppTest.java
+++ src/test/java/org/factoriaf5/AppTest.java
@@ -1,14 +1,37 @@
 package org.factoriaf5;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class AppTest 
 {
     @Test
-    public void shouldAnswerWithTrue()
+    public void esDoublePositivoTest()
     {
-        assertTrue( true );
+        assertTrue(App.esDoublePositivo("1"));
+        assertFalse(App.esDoublePositivo("0"));
+        assertFalse(App.esDoublePositivo(""));
     }
+    @Test
+    public void calculaIMCTest()
+    {
+        Double peso = 53.88;
+        Double altura = 1.95;
+        Double imc = 14.16;
+        assertTrue(App.calcularIMC(peso, altura).equals(imc));
+    }
+    @Test
+    public void evaluaResultadoIMCTest()
+    {
+        assertTrue(App.evaluarResultadoIMC(15.0).equals("delgadez severa"));
+        assertTrue(App.evaluarResultadoIMC(16.0).equals("delgadez moderada"));
+        assertTrue(App.evaluarResultadoIMC(17.0).equals("delgadez leve"));
+        assertTrue(App.evaluarResultadoIMC(18.5).equals("peso normal"));
+        assertTrue(App.evaluarResultadoIMC(25.0).equals("sobrepeso"));
+        assertTrue(App.evaluarResultadoIMC(30.0).equals("obesidad leve"));
+        assertTrue(App.evaluarResultadoIMC(35.0).equals("obesidad moderada"));
+        assertTrue(App.evaluarResultadoIMC(40.0).equals("obesidad morbida"));
+    }
+
 }
